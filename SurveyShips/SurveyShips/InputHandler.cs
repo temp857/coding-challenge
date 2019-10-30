5a5c20b7e01c9c0e3bc09aea1549620b27fa0b56
Matt Bond <matt.v.bond@gmail.com>
2019-10-30 19:00:43 +0000

    Add some argument checking to InputHandler

--- SurveyShips/SurveyShips/InputHandler.cs
+++ SurveyShips/SurveyShips/InputHandler.cs
@@ -8,7 +8,39 @@ namespace SurveyShips
     {
         public static string[] ProcessInput(string[] input)
         {
+            if (input == null)
+            {
+                throw new ArgumentException("Input should not be null!");
+            }
+
+            string[] filteredInput = filterInput(input);
+
+            if ((filteredInput.Length % 2) != 1)
+            {
+                throw new ArgumentException("Input should have odd number of elements!");
+            }
+
             return new string[0];
+        }
+
+        /// <summary>
+        /// Filter the input, removing any blank lines.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private static string[] filterInput(string[] input)
+        {
+            List<string> filteredInput = new List<string>();
+
+            foreach (string line in input)
+            {
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    filteredInput.Add(line);
+                }
+            }
+
+            return filteredInput.ToArray();
         }
     }
 }
